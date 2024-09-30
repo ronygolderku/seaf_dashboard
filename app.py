279637f51ad81047b65e252630c69ac7882f9424
ronygolderku <ronygolderku@gmail.com>
2024-09-30 23:42:16 +0800

    test1

--- app.py
+++ app.py
@@ -13,6 +13,6 @@ app.layout = layout
 register_callbacks(app)
 
 # Run the server
-if __name__ == "__main__":
-    app.run_server(debug=True)
+if __name__ == '__main__':
+    app.run_server(debug=True, host='0.0.0.0', port=8000)
 
